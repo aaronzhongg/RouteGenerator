5b179153396d8b42180f777f9b0c4357ab605658
Aaron Zhong <Aaron Zhong>
2017-07-09 12:50:08 +1200

    Change to make route return back to origin
    The points-of-interest are used as waypoints. The routes which are
    generated go from the origin to the point-of-interest back to the origin
    along the same path.

--- RouteGenerator/Controllers/DirectionController.cs
+++ RouteGenerator/Controllers/DirectionController.cs
@@ -16,28 +16,28 @@ namespace RouteGenerator.Controllers
     {
         static String googleMapsApiKey = "AIzaSyBUbdzIVx06BmduTI3KNeFuABPziBb6bTY";
         String googleMapsBaseUrl = "https://maps.googleapis.com";
+        String placesApiPathUrl;
+        String directionsApiPathUrl;
 
-        // TODO: Next step is to take a distance and current location as input, call Google Places API to get some POIs and find a POI that is of the desired (or close to) that
-        // of the input distance
         [HttpGet]
         public async Task<IHttpActionResult> GenerateRoute(int inputDistance, String latlng)
         {
-            var client = new HttpClient();
             String origin = latlng;
-            String destination = null;
+            String destination = latlng;
 
-            String placesApiPathUrl = "/maps/api/place/nearbysearch/json?location=" + latlng + "&radius=" + inputDistance + "&type=park&key=" + googleMapsApiKey; // Search a radius of the inputDistance for POIs
+            placesApiPathUrl = SetPlacesApiPathUrl(latlng, inputDistance.ToString()); // Search a radius of the inputDistance for POIs
 
             GoogleDirectionsObject.RootObject googleDirectionObject = null;
             GooglePlacesObject.RootObject googlePlacesObject = null;
 
+            // Set up HttpClient to call API
+            var client = new HttpClient();
             client.BaseAddress = new Uri(googleMapsBaseUrl);
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            HttpResponseMessage response = await client.GetAsync(placesApiPathUrl);
-
             // Get POIs 
+            HttpResponseMessage response = await client.GetAsync(placesApiPathUrl);
             if (response.IsSuccessStatusCode)
             {
                 googlePlacesObject = await response.Content.ReadAsAsync<GooglePlacesObject.RootObject>();
@@ -49,8 +49,9 @@ namespace RouteGenerator.Controllers
             //Query each POI to find one that is close to the user's input distance
             foreach (GooglePlacesObject.Result r in googlePlacesObject.results)
             {
-                destination = r.geometry.location.lat + "," + r.geometry.location.lng;
-                String directionsApiPathUrl = "/maps/api/directions/json?origin=" + origin + "&destination=" + destination + "&mode=walking&key=" + googleMapsApiKey;
+                // Using the returned POI as a waypoints
+                String waypoints = r.geometry.location.lat + "," + r.geometry.location.lng;
+                directionsApiPathUrl = SetDirectionsApiPathUrl(origin, destination, waypoints);
                 response = await client.GetAsync(directionsApiPathUrl);
 
                 if (response.IsSuccessStatusCode)
@@ -66,8 +67,8 @@ namespace RouteGenerator.Controllers
                             currentRouteTotalDistance += leg.distance.value;
                         }
 
-                        // If the route is within 250m of inputDistance then return, otherwise return the one with the closet distance
-                        if (Math.Abs(currentRouteTotalDistance - inputDistance) < 250)
+                        // If the route is within 200m of inputDistance then return, otherwise return the one with the closet distance
+                        if (Math.Abs(currentRouteTotalDistance - inputDistance) < 200)
                         {
                             return Ok(route);
                         }
@@ -86,6 +87,16 @@ namespace RouteGenerator.Controllers
             }
 
             return Ok(returnRoute);
+        }
+
+        private String SetDirectionsApiPathUrl(String origin, String destination, String waypoints)
+        {
+            return "/maps/api/directions/json?origin=" + origin + "&destination=" + destination + "&mode=walking&waypoints=" + waypoints + "&key=" + googleMapsApiKey;
+        }
+
+        private String SetPlacesApiPathUrl(String latlng, String radius)
+        {
+            return "/maps/api/place/nearbysearch/json?location=" + latlng + "&radius=" + radius + "&type=park&key=" + googleMapsApiKey;
         }
     }
 }
